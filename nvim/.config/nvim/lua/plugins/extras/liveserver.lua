33d152116d5e04bac4ca1f7387638553e39a1f93
dg-fuadmhnr <fuad@webdeveloper.dgtraffic.com>
2025-02-27 20:22:16 +0700

    [feat] add indentscoop, liveserver and neoscroll
    indentscoop to add color based on indentation, liveserver and neoscrool
    for smooth scroll

---
+++ nvim/.config/nvim/lua/plugins/extras/liveserver.lua
@@ -0,0 +1,9 @@
+return {
+	"barrett-ruth/live-server.nvim",
+	build = "npm i -g live-server",
+	cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
+	config = true,
+	keys = {
+		{ "<leader>rl", "<cmd>LiveServerToggle<cr>", desc = "Live Server" },
+	},
+}
