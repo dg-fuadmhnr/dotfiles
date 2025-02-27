572796f1ef14f93ebb59166d9ef6ce3d24ecea16
dg-fuadmhnr <fuad@webdeveloper.dgtraffic.com>
2025-02-28 04:56:50 +0700

    [feat] add keymaps to bufferline and filebrowser

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -30,5 +30,116 @@ return {
 		{ "<leader>bD", "<cmd>BufferLineCloseRight<cr>", desc = "Close Buffer Right", mode = "n" },
 		{ "<leader>ba", "<cmd>BufferLineCloseOthers<cr>", desc = "Close Buffer Other", mode = "n" },
 		{ "<leader>bA", "<cmd>BufferLineCloseOthers<cr><cmd>bd!<cr>", desc = "Close Buffer All", mode = "n" },
+		-- Add keybindings for leader b0, b1, b2, etc.
+		{
+			"<leader>b0",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 0 then
+					vim.cmd("buffer " .. buflist[1]) -- Select the first buffer (index 1)
+				end
+			end,
+			desc = "Select Buffer 1",
+			mode = "n",
+		},
+		{
+			"<leader>b1",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 1 then
+					vim.cmd("buffer " .. buflist[2]) -- Select the second buffer (index 2)
+				end
+			end,
+			desc = "Select Buffer 2",
+			mode = "n",
+		},
+		{
+			"<leader>b2",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 2 then
+					vim.cmd("buffer " .. buflist[3]) -- Select the third buffer (index 3)
+				end
+			end,
+			desc = "Select Buffer 3",
+			mode = "n",
+		},
+		{
+			"<leader>b3",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 3 then
+					vim.cmd("buffer " .. buflist[4])
+				end
+			end,
+			desc = "Select Buffer 4",
+			mode = "n",
+		},
+		{
+			"<leader>b4",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 4 then
+					vim.cmd("buffer " .. buflist[5])
+				end
+			end,
+			desc = "Select Buffer 5",
+			mode = "n",
+		},
+		{
+			"<leader>b5",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 5 then
+					vim.cmd("buffer " .. buflist[6])
+				end
+			end,
+			desc = "Select Buffer 6",
+			mode = "n",
+		},
+		{
+			"<leader>b6",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 6 then
+					vim.cmd("buffer " .. buflist[7])
+				end
+			end,
+			desc = "Select Buffer 7",
+			mode = "n",
+		},
+		{
+			"<leader>b7",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 7 then
+					vim.cmd("buffer " .. buflist[8])
+				end
+			end,
+			desc = "Select Buffer 8",
+			mode = "n",
+		},
+		{
+			"<leader>b8",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 8 then
+					vim.cmd("buffer " .. buflist[9])
+				end
+			end,
+			desc = "Select Buffer 9",
+			mode = "n",
+		},
+		{
+			"<leader>b9",
+			function()
+				local buflist = vim.api.nvim_list_bufs()
+				if #buflist > 9 then
+					vim.cmd("buffer " .. buflist[10])
+				end
+			end,
+			desc = "Select Buffer 10",
+			mode = "n",
+		},
 	},
 }
